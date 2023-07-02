6d780e5a59450285e99ea4a89dc2d7303d263ff6
Masoud Kian <masoudkiannezhad@gmail.com>
2023-07-03 01:49:31 +0330

    Set FireBase in Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_messenger/firebase_options.dart';
 
-void main() {
+void main()async {
+  //
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -9,6 +15,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: Scaffold(),
+    );
   }
 }
